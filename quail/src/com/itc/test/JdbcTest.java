07bf3242ca62fd5bfb1bcfd2276609bfd4ee0526
fanzhw <903520246@qq.com>
2014-10-31 13:40:22 +0800

    jdbc

--- quail/src/com/itc/test/JdbcTest.java
+++ quail/src/com/itc/test/JdbcTest.java
@@ -12,6 +12,9 @@ public class JdbcTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 		UserService service = (UserService) context.getBean("userService");
 		List<User> list = service.getUsers();
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i).getName()+"       "+list.get(i).getAge());
+		}
 		System.out.println(list.size());
 	}
 }
